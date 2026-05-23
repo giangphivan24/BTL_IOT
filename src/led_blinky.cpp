cafbdd3047e89e77690b46f61534e2a4e6a4c0c6
Hoang-Viet-Tran <hoang.tranviethoang1@hcmut.edu.vn>
2026-05-23 20:21:18 +0700

    Update led_blinky.cpp

--- src/led_blinky.cpp
+++ src/led_blinky.cpp
@@ -1,12 +1,29 @@
 #include "led_blinky.h"
+#include "global.h"
 
-void led_blinky(void *pvParameters){
+void led_blinky(void *pvParameters)
+{
   pinMode(LED_GPIO, OUTPUT);
-  
-  while(1) {                        
-    digitalWrite(LED_GPIO, HIGH);  // turn the LED ON
-    vTaskDelay(1000);
-    digitalWrite(LED_GPIO, LOW);  // turn the LED OFF
-    vTaskDelay(1000);
+
+  SensorData data;
+  int delayTime = 1000;
+
+  while (1)
+  {
+    if (xQueueReceive(queueLED, &data, portMAX_DELAY))
+    {
+      if (data.temp < 25)
+        delayTime = 1000;
+      else if (data.temp < 35)
+        delayTime = 500;
+      else
+        delayTime = 100;
+    }
+
+    digitalWrite(LED_GPIO, HIGH);
+    vTaskDelay(pdMS_TO_TICKS(delayTime));
+
+    digitalWrite(LED_GPIO, LOW);
+    vTaskDelay(pdMS_TO_TICKS(delayTime));
   }
 }
