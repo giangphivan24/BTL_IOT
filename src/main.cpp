2e83f9c19c27cd21ea0e043f15650dad776deca3
NhatTiens <tien.bui2912@hcmut.edu.vn>
2026-05-23 20:52:11 +0700

    Add WebInit task for AP mode and web server

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,11 @@ void setup()
   xTaskCreate(neo_blinky, "Task NEO Blink", 2048, NULL, 2, NULL);
   xTaskCreate(temp_humi_monitor, "Task TEMP HUMI Monitor", 2048, NULL, 2, NULL);
   // xTaskCreate(main_server_task, "Task Main Server" ,8192  ,NULL  ,2 , NULL);
+  xTaskCreate([](void*){
+    startAP();        // Task 4 yêu cầu AP mode
+    connnectWSV();    // chạy web server
+    vTaskDelete(NULL);
+}, "WebInit", 4096, NULL, 1, NULL);
   xTaskCreate(tiny_ml_task, "Tiny ML Task", 8192, NULL, 2, NULL);
   xTaskCreate(coreiot_task, "CoreIOT Task" ,4096  ,NULL  ,2 , NULL);
   xTaskCreate(Task_Toogle_BOOT, "Task_Toogle_BOOT", 4096, NULL, 2, NULL);
